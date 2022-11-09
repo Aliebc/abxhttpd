eeb9917e7092c584df55434678c911e527efdd23
TTDiang <TTDiang@outlook.com>
2022-11-10 00:16:35 +0800

    add 2 header files

---
+++ THUer/include/user.h
@@ -0,0 +1,17 @@
+#include <cstring>
+
+enum Rank{peasent = 1, elite, crazy, admin};
+
+class User{
+private:
+    Rank;
+
+    static bool CheckIfAlready(const string & Username);
+public:
+    User(const string & Username){}
+    User(){}
+    ~User(){}
+    static bool RegisterNewUser(const string & Username, string & password);
+    static bool Verify(const string & Username, string & password);
+
+};
